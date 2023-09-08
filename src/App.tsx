7d41d658e08bd1e2804721f707624f8413fe564c
Dominique Klevinghaus <d.klevinghaus@sonnen.de>
2023-09-08 15:44:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h1>Vitest inside Github Actions test project</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
